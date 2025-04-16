13cc8f1a02932efe709e2788c375488d806c6cf6
Favio <1507623@senati.pe>
2025-04-16 11:43:14 -0500

    Actualizacion modulo Estrella

---
+++ llamadoFuncion.js
@@ -0,0 +1,10 @@
+// Importamos el archivo que contiene las funiones
+const modulo = require('./MuestraTarifa');
+
+const nombreU = "Estrella";
+const apellidoU = "Dominguez";
+
+
+modulo.MostrarTarifarioCarros(nombreU,apellidoU)
+
+
